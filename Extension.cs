21a45b1de2a633d474ee95467858385147026306
Your Name <you@example.com>
2022-03-01 10:09:06 +0400

    Add project files.

---
+++ Extension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hometask3
+{
+    static class Extension
+    {
+        public static string IsCapitalized(this string word)
+        {
+            string word1 = "";
+            word = word.ToLower();
+            string[] words = word.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i][0].ToString().ToUpper() + words[i].Substring(1);
+            }
+            word1 = string.Join(' ', words);
+            return word1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace hometask3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string word = "exTensiON claSSi staTic OLMalidiR";
+                Console.WriteLine(word.IsCapitalized());
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
